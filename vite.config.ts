48996b4d2b32a6d69b07665951040fe4cb9c3ffc
Federico Mujica <federicomujica1@gmail.com>
2024-05-23 16:18:34 +0200

    d

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ export default ({ mode }) => {
 	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 	return defineConfig({
 		define: {
-			'process.<wbr>env': {NODE_ENV:"production"}
+			'process.<wbr>env': {NODE_ENV:"production"},
+			'process.env.NODE_ENV':"production"
 			
 		},
 		plugins: [react(), basicSsl({})],
