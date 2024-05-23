938b4b9f5639dcee907497c106faf8c2d24495c2
Federico Mujica <federicomujica1@gmail.com>
2024-05-23 16:03:52 +0200

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,25 @@
-import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react'
 import basicSsl from '@vitejs/plugin-basic-ssl'
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	define: {
-		'process.<wbr>env': {}
-	},
-	plugins: [react(),basicSsl({})],
-	build: {
-		lib: {
-			formats: ['es'],
-			fileName: process.env.npm_config_component,
-			entry: `./src/components/${process.env.npm_config_component}/index.tsx`,
-
+export default ({ mode }) => {
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+	return defineConfig({
+		define: {
+			'process.env.NODE_ENV': process.env.NODE_ENV,
+			'process.<wbr>env': {},
+			
 		},
+		plugins: [react(), basicSsl({})],
+		build: {
+			lib: {
+				formats: ['es'],
+				fileName: process.env.npm_config_component,
+				entry: `./src/components/${process.env.npm_config_component}/index.tsx`,
 
-	}
+			},
 
-})
+		}
+	})
+}
